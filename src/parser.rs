7505de8729a0bf7210cb1822dbc53f4363f02217
OnofreTZK <taraujonofre@gmail.com>
2023-09-11 21:25:48 -0300

    refactored the pre process function

--- src/parser.rs
+++ src/parser.rs
@@ -1,27 +1,40 @@
+use std::str::Lines;
+
 trait ParserStringOps {
-    fn remove_new_lines(&self) -> String;
     fn remove_marked_emoji(&self) -> String;
 }
 
 impl ParserStringOps for String {
-    fn remove_new_lines(&self) -> String {
-        self.replace("\n", "")
-    }
-
     fn remove_marked_emoji(&self) -> String {
         self.replace("✅", "")
-    }
+    }    
 }
 
-fn pre_process(raw_content: &str) -> String {
-    //remove_marked_emoji(&remove_new_lines(raw_content))
-    String::from(raw_content)
-        .remove_new_lines()
-        .remove_marked_emoji()
+// Removes the green marked emoji, the numbers and trim the whitespaces
+fn pre_process(raw_content: Lines) -> Vec<String> {
+    raw_content
+        .map(|line| -> String { line.replace("✅", "") })
+        .map(|line| -> String {
+            let filtered_names: Vec<&str> = line.split(".").collect();
+            match filtered_names.last() {
+                Some(name) => {
+                    return name.to_string();
+                }
+                None => {
+                    return "".to_string();
+                }
+            }
+        })
+        .map(|line| -> String { line.trim().to_string() })
+        .collect()
 }
 
 pub fn run(content: &str) {
-    let processed: &str = &pre_process(content);
+    let lines: Lines = content.lines();
 
-    println!("starting the parser\n\n{}", processed);
+    let parsed_lines: Vec<String> = pre_process(lines);
+
+    for line in parsed_lines {
+        println!("{}", line)
+    }
 }
