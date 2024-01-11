40a7fcf27beb7437b9946cb20a1df2c5485acf8a
OnofreTZK <taraujonofre@gmail.com>
2024-01-10 21:09:57 -0300

    handling empty category

--- src/pelada.rs
+++ src/pelada.rs
@@ -1,9 +1,10 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
-const GOALKEEPER: [&str; 4] = ["goleiros", "goleiro", "gol", "arqueiros"];
-const PLAYER: [&str; 3] = ["jogadores", "jogador", "linha"];
-const GUEST: [&str; 8] = [
+const GOALKEEPER: [&str; 5] = ["goalkeepers", "goleiros", "goleiro", "gol", "arqueiros"];
+const PLAYER: [&str; 4] = ["players", "jogadores", "jogador", "linha"];
+const GUEST: [&str; 9] = [
+    "guests",
     "suplentes/convidados",
     "suplentes",
     "convidados",
@@ -13,7 +14,7 @@ const GUEST: [&str; 8] = [
     "reservas",
     "reserva",
 ];
-const KID: [&str; 3] = ["sub 15", "menores", "menor"];
+const KID: [&str; 5] = ["kids", "crianças", "sub 15", "menores", "menor"];
 
 #[derive(Serialize, Deserialize)]
 pub struct PeladaType {
@@ -31,12 +32,21 @@ impl PeladaType {
     }
 }
 
+// Functions
+// =======================================================================
 fn list_to_lower(list: Vec<String>) -> Vec<String> {
     list.iter()
         .map(|word| -> String {
             return word.to_lowercase();
         })
         .collect()
+}
+
+fn empty_list_warning(keyword: &str) {
+    println!(
+        "Please, check the list! I couldn't parse the {} keyword. Assigning an empty list.",
+        keyword
+    );
 }
 
 fn get_keyword(group: &[&str], list: &Vec<String>) -> Option<String> {
@@ -47,74 +57,53 @@ fn get_keyword(group: &[&str], list: &Vec<String>) -> Option<String> {
             return Some(keyword);
         }
     }
+
+    let group_name = match group.get(0) {
+        Some(name) => name,
+        None => "",
+    };
+    empty_list_warning(group_name);
     None
 }
 
-fn keywords(list: &Vec<String>) -> (String, String, String, String) {
-    let gk_keyword: String;
-    let pl_keyword: String;
-    let gt_keyword: String;
-    let kd_keyword: String;
-
-    match get_keyword(&GOALKEEPER, &list) {
-        Some(keyword) => {
-            gk_keyword = keyword;
-        }
-        None => {
-            panic!("Please, check the list! I couldn't parse the goalkeeper keyword.");
-        }
-    }
-
-    match get_keyword(&PLAYER, &list) {
-        Some(keyword) => {
-            pl_keyword = keyword;
-        }
-        None => {
-            panic!("Please, check the list! I couldn't parse the player keyword.");
-        }
-    }
-
-    match get_keyword(&GUEST, &list) {
-        Some(keyword) => {
-            gt_keyword = keyword;
-        }
-        None => {
-            panic!("Please, check the list! I couldn't parse the guest keyword.");
-        }
-    }
-
-    match get_keyword(&KID, &list) {
-        Some(keyword) => {
-            kd_keyword = keyword;
-        }
-        None => {
-            panic!("Please, check the list! I couldn't parse the kid keyword.");
-        }
-    }
+fn keywords(
+    list: &Vec<String>,
+) -> (
+    Option<String>,
+    Option<String>,
+    Option<String>,
+    Option<String>,
+) {
+    let gk_keyword = get_keyword(&GOALKEEPER, &list);
+    let pl_keyword = get_keyword(&PLAYER, &list);
+    let gt_keyword = get_keyword(&GUEST, &list);
+    let kd_keyword = get_keyword(&KID, &list);
 
     (gk_keyword, pl_keyword, gt_keyword, kd_keyword)
 }
 
-fn get_keyword_index(keyword: &String, list: &Vec<String>) -> usize {
-    match list.iter().position(|word| word == keyword) {
-        Some(index) => {
-            return index;
+fn get_keyword_index(keyword_opt: &Option<String>, list: &Vec<String>) -> Option<usize> {
+    match keyword_opt {
+        Some(keyword) => {
+            list.iter().position(|word| word == keyword)
+            //match list.iter().position(|word| word == keyword) {
+            //    Some(index) => {
+            //        return index;
+            //    }
+            //    None => {
+            //        panic!("keyword {} not found in the list!!", keyword);
+            //    }
+            //}
         }
-        None => {
-            panic!("keyword {} not found in the list!!", keyword);
-        }
+        None => None,
     }
 }
 
-fn sublist(from: &String, to: &String, list: &Vec<String>) -> Vec<String> {
-    let from_index: usize = get_keyword_index(from, list) + 1;
-
-    if to.is_empty() {
-        return list[from_index..(list.len())].to_vec();
-    } else {
-        let to_index: usize = get_keyword_index(to, list);
-
-        return list[from_index..to_index].to_vec();
+fn sublist(from: Option<usize>, to: Option<usize>, list: &Vec<String>) -> Vec<String> {
+    match (from, to) {
+        (Some(from_index), Some(to_index)) => return list[from_index..to_index].to_vec(),
+        (Some(from_index), None) => return list[from_index..(list.len())].to_vec(),
+        (None, _) => return [].to_vec(),
     }
 }
 
@@ -123,11 +112,18 @@ pub fn from(list: Vec<String>) -> PeladaType {
 
     let (gk_keyword, pl_keyword, gt_keyword, kd_keyword) = keywords(&lowered_list);
 
+    let keywords_list: [Option<String>; 4] = [gk_keyword, pl_keyword, gt_keyword, kd_keyword];
+
+    let indices: Vec<Option<usize>> = keywords_list
+        .iter()
+        .map(|keyword_opt| get_keyword_index(keyword_opt, &lowered_list))
+        .collect();
+
     // TODO: Could be parallel
-    let gk = sublist(&gk_keyword, &pl_keyword, &lowered_list);
-    let pl = sublist(&pl_keyword, &gt_keyword, &lowered_list);
-    let gt = sublist(&gt_keyword, &kd_keyword, &lowered_list);
-    let kd = sublist(&kd_keyword, &String::from(""), &lowered_list);
+    let gk = sublist(Some(1), Some(2), &lowered_list);
+    let pl = sublist(Some(2), Some(3), &lowered_list);
+    let gt = sublist(Some(3), Some(4), &lowered_list);
+    let kd = sublist(Some(4), None, &lowered_list);
 
     PeladaType {
         goalkeepers: gk,
